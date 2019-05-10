9df69a9577eaa2d547525a3e3f0f581c78ebe7da
Binu Judith J T <41480891+binujudith@users.noreply.github.com>
2019-05-10 12:56:52 +0530

    playing with characters

---
+++ playing with characters.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    char ch;
+    char s[10];
+    char sen[100];
+    scanf("%c%*c",&ch);
+    scanf("%s%*c",s);
+    scanf("%[^\n]%*c",sen);
+    printf("%c\n",ch);
+    printf("%s\n",s);
+    printf("%s",sen);
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    return 0;
+}
+
